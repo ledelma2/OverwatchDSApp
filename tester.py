41b50408b1b6f26786c200dff3b42350d7374657
ledelma2 <ledelma2@uic.edu>
2018-11-16 14:13:27 -0600

    Database Update and Additon of Tester.py

---
+++ tester.py
@@ -0,0 +1,8 @@
+#overwatch data retriever
+#utilizes both the python-overwatch api and beautiful soup to retrive data
+from bs4 import BeautifulSoup
+from overwatch import Overwatch
+
+
+overwatch = Overwatch(battletag="Serb#11472")
+print(overwatch(mode="cp", hero="ana", filter="assists"))
