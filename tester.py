fc3515eca6fd59ac1080a9f45e46862585124ca7
ledelma2 <ledelma2@uic.edu>
2019-01-24 13:31:50 -0600

    Added pyodbc Module to access DB.accdb

--- tester.py
+++ tester.py
@@ -2,7 +2,20 @@
 #utilizes both the python-overwatch api and beautiful soup to retrive data
 from bs4 import BeautifulSoup
 from overwatch import Overwatch
+import pyodbc
 
 
 overwatch = Overwatch(battletag="Serb#11472")
-print(overwatch(mode="cp", hero="ashe", filter="combat"))
+print(overwatch(mode="cp", hero="ana", filter="game"))
+db_file = r'''C:\Users\Liam Edelman\Documents\Personal\OverwatchDSApp\DB.accdb'''
+user = 'admin'
+password = ''
+odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;UID=%s;PWD=%s' %\
+                (db_file, user, password)
+print('connecting to db...')
+conn = pyodbc.connect(odbc_conn_str)
+crsr = conn.cursor()
+for table_info in crsr.tables():
+    print(table_info.table_name)
+print('closing db...')
+conn.close()
